186240bb688b4783081cff0b2a87c3ae0e786921
Aras Kayvanrad <aras.kayvanrad@mindbridge.ai>
2020-04-12 20:14:05 -0400

    simplified cum_gains reimplementation

--- metrics.py
+++ metrics.py
@@ -1,74 +1,46 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from typing import Tuple
 
-def cummulative_gains(y_true, y_scores, df=None, ascending = False,
-                      show_plot = True, ax = None):
+def cum_gains(y_true: np.array, y_score: np.array,
+              ascending: bool = False) -> Tuple[np.array, np.array, float]:
     '''
-    Plot cummulative gains charts and calculate the area under the curve.
+    Calculate cummulative gains charts and the area under the curve.
 
     Parameters
     ----------
-    y_true: numpy array or string
-        Numpy array containing true labels or, if data given in a dataframe,
-        a string containing the column name of the true labels.
-    y_scores: numpy array or string or list of strings
-        Numpy array containing scores (e.g., probabilities), or, if data given
-        in a dataframe, a string containing the column name of the scores, or,
-        if multiple scores present, a list of strings containing column names
-        of the scores.
-    df: pandas dataframe
-        Dataframe containg the data. If None, data must be given in two numpy
-        arrays (y_true and y_scores).
-    ascending: boolean
+    y_true: array_like
+        True binary labels
+    y_score: array_like
+        Target scores
+    ascending: bool
         Sort scores ascending
-    show_plot: boolean
-        Show cummulative gains charts
-    ax: matplotlib.axes.Axes object
-        Axes to plot the the cummulative gains charts on. If None, new plot will
-        be created.
 
     Returns
     -------
-    auc: double or dictionary
-        Area under cummulative gains chart(s). If multiple score columns
-        present, a dictionary of score column names will be returned containing
-        the AUC for each column.
+    frac_samples: array
+        Increasing fraction of samples
+    response_rage: array
+        Increasing response rate
+    auc: float
+        area under curve
     '''
-    if df is None: # y_true and y_scores provided as vectors
-        df = pd.DataFrame({'y_true':y_true, 'y_scores':y_scores})
-        df = df.sort_values(by='y_scores', ascending=ascending)
-        df['response_rate'] = df['y_true'].cumsum()/df['y_true'].sum()
-        df['frac_samples'] = df['y_scores'].rank(ascending = ascending)/len(df)
-        if show_plot:
-            if ax is None:
-                fig, ax = plt.subplots()
-            ax.plot(df['frac_samples'], df['response_rate'])
-            ax.plot([0,1],[0,1], '--')
-        return np.trapz(df['response_rate'], df['frac_samples'])
-    else: # data provided in a pandas dataframe
-        if isinstance(y_scores, list): # (multiple) score columns given in a list
-            if show_plot and ax is None:
-                fig, ax = plt.subplots()
-            ax.plot([0,1],[0,1], '--')
-            auc = dict()
-            for i, score_col in enumerate(y_scores):
-                df = df.sort_values(by=score_col, ascending=ascending)
-                df['response_rate_{}'.format(i)] = df[y_true].cumsum()/df[y_true].sum()
-                df['frac_samples_{}'.format(i)] = df[score_col].rank(ascending = ascending)/len(df)
-                if show_plot:
-                    ax.plot(df['frac_samples_{}'.format(i)], df['response_rate_{}'.format(i)],
-                            color='C{}'.format(i), label=score_col)
-                auc[score_col] = np.trapz(df['response_rate_{}'.format(i)], df['frac_samples_{}'.format(i)])
-            plt.legend()
-            return(auc)
-        else: # single score column given as a string
-            df = df.sort_values(by=y_scores, ascending=ascending)
-            df['response_rate'] = df[y_true].cumsum()/df[y_true].sum()
-            df['frac_samples'] = df[y_scores].rank(ascending = ascending)/len(df)
-            if show_plot:
-                if ax is None:
-                    fig, ax = plt.subplots()
-                ax.plot(df['frac_samples'], df['response_rate'])
-                ax.plot([0,1],[0,1], '--')
-            return np.trapz(df['response_rate'], df['frac_samples'])
+    # check length of input arrays
+    if len(y_true) != len(y_score):
+        raise ValueError('y_true and y_score must have equal length')
+
+    # convert array_like to numpy arrays
+    y_true = np.array(y_true)
+    y_score = np.array(y_score)
+
+    # sort by y_score
+    if not ascending:
+        y_score = -y_score
+    ranking = y_score.argsort()
+    y_true = y_true[ranking]
+    y_score = y_score[ranking]
+
+    response_rate = np.cumsum(y_true) / np.sum(y_true)
+    frac_samples = np.arange(len(y_score))[ranking] / len(y_score)
+    auc = np.trapz(response_rate, frac_samples)
+
+    return (frac_samples, response_rate, auc)
--- test_metrics.py
+++ test_metrics.py
@@ -1,36 +1,29 @@
 import unittest
 import metrics
-import pandas as pd
 
 class TestMetrics(unittest.TestCase):
-    def setUp(self):
-            self.labels = [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
-                           1, 0]
-            self.scores1 = [0.9, 0.8, 0.7, 0.6, 0.55, 0.51, 0.49, 0.43, 0.42,
-                           0.39, 0.33, 0.31, 0.23, 0.22, 0.19, 0.15, 0.12, 0.11,
-                           0.04, 0.01]
-            self.scores2 = [0.9, 0.8, 0.07, 0.6, 0.55, 0.51, 0.49, 0.43, 0.42,
-                           0.39, 0.33, 0.31, 0.23, 0.22, 0.19, 0.15, 0.12, 0.11,
-                           0.84, 0.01]        
-            
-            self.df = pd.DataFrame({'label':self.labels,
-                                    'scores1':self.scores1,
-                                    'scores2':self.scores2})
 
-    def test_cummulative_gains_array_input(self):
-        self.assertAlmostEqual(metrics.cummulative_gains(self.labels,
-                                                         self.scores1),
-                               0.5875, places=4)
+    def test_cum_gains_typical_input(self):
+        labels = [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]
+        scores = [0.9, 0.8, 0.7, 0.6, 0.55, 0.51, 0.49, 0.43, 0.42,
+                  0.39, 0.33, 0.31, 0.23, 0.22, 0.19, 0.15, 0.12, 0.11,
+                  0.04, 0.01]        
+        _,_,auc = metrics.cum_gains(labels, scores)
+        self.assertAlmostEqual(auc, 0.5875, places=4)
+
+    def test_cum_gains_empty_inputs(self):
+        frac_samples, response_rate, auc = metrics.cum_gains([],[])
+        self.assertEqual(frac_samples.size, 0)
+        self.assertEqual(response_rate.size, 0)
+        self.assertEqual(auc, 0.0)
+
+    def test_cum_gains_unequal_length_inputs(self):
+        self.assertRaises(ValueError, metrics.cum_gains, [1], [2,3])
+
+
+
     
-    def test_cummulative_gains_df_input_single_score(self):
-        self.assertAlmostEqual(metrics.cummulative_gains('label', 'scores1',
-                                                         self.df),
-                               0.5875, places=4)
 
-    def test_cummulative_gains_df_input_mutiple_socres(self):
-        res = metrics.cummulative_gains('label', ['scores1', 'scores2'], self.df)
-        self.assertAlmostEqual(res['scores1'], 0.5875, places=4)
-        self.assertAlmostEqual(res['scores2'], 0.6675, places=4)
 
 if __name__ == '__main__':
     unittest.main()
